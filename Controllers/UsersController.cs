0e811e9ebae306570571abf271cf23992b45d8c9
Gaston Mansilla <gastonmansilla9@gmail.com>
2023-07-04 01:02:23 -0300

    agregue botones navegacion Users y Roles

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,63 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using SegundoParcial.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using SegundoParcial.ViewModels;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using SegundoParcial.Controllers;
+
+namespace SegundoParcial.Controllers;
+
+
+public class UsersController : Controller
+{
+   
+    private readonly ILogger<HomeController> _logger;
+    private readonly UserManager<IdentityUser> _userManager;
+    private readonly RoleManager<IdentityRole> _roleManager;
+
+    public UsersController(
+        ILogger<HomeController> logger,
+        UserManager<IdentityUser> userManager,
+        RoleManager<IdentityRole> roleManager)
+    {
+        _logger = logger;
+        _userManager = userManager;
+        _roleManager = roleManager;
+    }
+
+    public IActionResult Index()
+    {
+        //listar todos los usuarios
+        var users = _userManager.Users.ToList();
+        return View(users);
+    }
+
+    
+    public async Task<IActionResult> Edit(string id)
+    {
+        var user = await _userManager.FindByIdAsync(id);
+        
+        var userViewModel = new UserEditViewModel();
+        userViewModel.UserName = user.UserName ?? string.Empty;
+        userViewModel.Email = user.Email ?? string.Empty;
+        userViewModel.Roles = new SelectList(_roleManager.Roles.ToList());
+
+        return View(userViewModel);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Edit(UserEditViewModel model)
+    {
+        var user = await _userManager.FindByNameAsync(model.UserName);
+        if (user != null)
+        {
+            await _userManager.AddToRoleAsync(user, model.Role);
+        }
+
+        return RedirectToAction("Index");
+    }
+
+    
+}
